d1f17667fc8ceac7c8b74075cc453a6eb79f7736
jjwon2149 <jjwon2149@gmail.com>
2025-03-23 23:25:15 +0900

    feat: Color+Extensions 추가 - 추후에 사용할 색상 저장 - hex값 지원

---
+++ SwypApp2nd/Sources/Common/Color+Extensions.swift
@@ -0,0 +1,25 @@
+import Foundation
+import SwiftUI
+
+extension Color {
+    // Hex값 색상 지원
+    init(hex: String) {
+        let hex = hex.trimmingCharacters(in: .whitespacesAndNewlines)
+        let scanner = Scanner(string: hex)
+        if hex.hasPrefix("#") {
+            _ = scanner.scanString("#")
+        }
+
+        var rgb: UInt64 = 0
+        scanner.scanHexInt64(&rgb)
+
+        let r = Double((rgb >> 16) & 0xFF) / 255
+        let g = Double((rgb >> 8) & 0xFF) / 255
+        let b = Double(rgb & 0xFF) / 255
+
+        self.init(red: r, green: g, blue: b)
+    }
+    
+    // TODO: - 사용할 색상 미리 저장
+    static let mainYellow = Color(hex: "#FFFFF")
+}
