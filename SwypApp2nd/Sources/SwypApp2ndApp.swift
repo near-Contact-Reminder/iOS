117bcf38209f67624c5537abb15315d6baf619f8
jjwon2149 <jjwon2149@gmail.com>
2025-03-25 18:35:47 +0900

    feat: APNs 추가 - AppDelegate를 통한 AppState 관리

--- SwypApp2nd/Sources/SwypApp2ndApp.swift
+++ SwypApp2nd/Sources/SwypApp2ndApp.swift
@@ -2,6 +2,8 @@ import SwiftUI
 
 @main
 struct SwypApp2ndApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+
     var body: some Scene {
         WindowGroup {
             ContentView()
