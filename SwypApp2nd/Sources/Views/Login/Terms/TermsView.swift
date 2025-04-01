1352890b81ae2cabc39a8388558241885110b532
jjwon2149 <jjwon2149@gmail.com>
2025-04-01 17:05:08 +0900

    feat: 약관 상세 페이지 추가 (진행중)

--- SwypApp2nd/Sources/Views/Login/Terms/TermsView.swift
+++ SwypApp2nd/Sources/Views/Login/Terms/TermsView.swift
@@ -1,8 +1,17 @@
 import SwiftUI
 import Combine
+import WebKit
 
 public struct TermsView: View {
     @ObservedObject var viewModel = TermsViewModel()
+    
+    // 약관 상세를 보여주기 위한 상태 관리
+    /// 약관 상세 모달 Bool
+    @State private var showingDetail = false
+    /// 약관 상세 타이틀
+    @State private var detailTitle = ""
+    /// 약관 상세 웹 URL
+    @State private var detailURLString = ""
     
     let completion: () -> Void
 
@@ -27,17 +36,20 @@ public struct TermsView: View {
                 agreementRow(
                     isChecked: $viewModel.isServiceTermsAgreed,
                     title: "서비스 이용 약관 상세",
-                    showDetail: true
+                    showDetail: true,
+                    detailURLString: "https://www.naver.com/"
                 )
                 agreementRow(
                     isChecked: $viewModel.isPersonalInfoTermsAgreed,
                     title: "개인정보 수집 및 이용 동의서 상세",
-                    showDetail: true
+                    showDetail: true,
+                    detailURLString: "https://www.naver.com/"
                 )
                 agreementRow(
                     isChecked: $viewModel.isPrivacyPolicyAgreed,
                     title: "개인정보 처리방침 상세",
-                    showDetail: true
+                    showDetail: true,
+                    detailURLString: "https://www.naver.com/"
                 )
             }
             .padding(.horizontal, 20)
@@ -46,7 +58,7 @@ public struct TermsView: View {
             Spacer()
 
             Button(action: {
-                /// 약관 동의 유무 UserDefaults에 저장
+                // MARK: - 약관 동의 유무 UserDefaults에 저장
                 UserDefaults.standard.set(true, forKey: "didAgreeToTerms")
                 completion()
             }) {
@@ -64,13 +76,39 @@ public struct TermsView: View {
         }
         .background(Color.white)
         .cornerRadius(24)
+        .fullScreenCover(isPresented: $showingDetail) {
+            NavigationStack {
+                TermsDetailView(
+                    title: detailTitle,
+                    urlString: detailURLString
+                )
+                .toolbar {
+                    ToolbarItem {
+                        HStack (alignment: .center) {
+                            Text(detailTitle)
+                                .font(.headline)
+                            
+                            Spacer()
+                            
+                            Button("X") {
+                                showingDetail = false
+                            }
+                            .foregroundStyle(Color.black)
+                        }
+                        
+                    }
+                }
+            }
+        }
     }
-
+    
+    // MARK: - 이용 약관 Row
     private func agreementRow(
         isChecked: Binding<Bool>,
         title: String,
         isBold: Bool = false,
         showDetail: Bool = false,
+        detailURLString: String? = nil,
         action: (() -> Void)? = nil
     ) -> some View {
         HStack {
@@ -91,14 +129,49 @@ public struct TermsView: View {
 
             Spacer()
 
-            if showDetail {
-                Image(systemName: "chevron.right")
-                    .foregroundColor(.gray)
+            if showDetail, let detailURLString = detailURLString {
+                Button {
+                    detailTitle = title
+                    self.detailURLString = detailURLString
+                    showingDetail = true
+                } label: {
+                    Image(systemName: "chevron.right")
+                        .foregroundColor(.gray)
+                }
             }
         }
         .padding()
         .background(Color(UIColor.systemGray6))
         .cornerRadius(8)
+    }
+}
+
+struct TermsDetailView: View {
+    let title: String
+    let urlString: String
+
+    var body: some View {
+        VStack {
+            if let url = URL(string: urlString) {
+                WebView(url: url)
+            } else {
+                Text("유효하지 않은 URL입니다.")
+                    .foregroundColor(.red)
+            }
+        }
+    }
+}
+
+struct WebView: UIViewRepresentable {
+    let url: URL
+
+    func makeUIView(context: Context) -> WKWebView {
+        return WKWebView()
+    }
+
+    func updateUIView(_ uiView: WKWebView, context: Context) {
+        let request = URLRequest(url: url)
+        uiView.load(request)
     }
 }
 
