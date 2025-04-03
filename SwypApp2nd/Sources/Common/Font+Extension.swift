ddd1ed0390193b4c8e8751a4e634659999278bc5
jjwon2149 <jjwon2149@gmail.com>
2025-04-03 17:07:48 +0900

    refactor: 폰트 사이즈 커스텀 추가 - 폰트 사이즈 커스텀 가능하도록 변경

--- SwypApp2nd/Sources/Common/Font+Extension.swift
+++ SwypApp2nd/Sources/Common/Font+Extension.swift
@@ -4,44 +4,44 @@ import SwiftUI
 
 extension Font {
     struct Pretendard {
-        static func h1Bold() -> Font {
-            Font.custom("Pretendard-Bold", size: 24)
+        static func h1Bold(size: CGFloat = 24) -> Font {
+            Font.custom("Pretendard-Bold", size: size)
         }
         
-        static func h1Medium() -> Font {
-            Font.custom("Pretendard-Medium", size: 24)
+        static func h1Medium(size: CGFloat = 24) -> Font {
+            Font.custom("Pretendard-Medium", size: size)
         }
         
-        static func h1Regular() -> Font {
-            Font.custom("Pretendard-Regular", size: 24)
+        static func h1Regular(size: CGFloat = 24) -> Font {
+            Font.custom("Pretendard-Regular", size: size)
         }
         
-        static func h2Bold() -> Font {
-            Font.custom("Pretendard-Bold", size: 18)
+        static func h2Bold(size: CGFloat = 18) -> Font {
+            Font.custom("Pretendard-Bold", size: size)
         }
         
-        static func b1Bold() -> Font {
-            Font.custom("Pretendard-Bold", size: 16)
+        static func b1Bold(size: CGFloat = 16) -> Font {
+            Font.custom("Pretendard-Bold", size: size)
         }
         
-        static func b1Medium() -> Font {
-            Font.custom("Pretendard-Medium", size: 16)
+        static func b1Medium(size: CGFloat = 16) -> Font {
+            Font.custom("Pretendard-Medium", size: size)
         }
         
-        static func b2Bold() -> Font {
-            Font.custom("Pretendard-Bold", size: 14)
+        static func b2Bold(size: CGFloat = 14) -> Font {
+            Font.custom("Pretendard-Bold", size: size)
         }
         
-        static func b2Medium() -> Font {
-            Font.custom("Pretendard-Medium", size: 14)
+        static func b2Medium(size: CGFloat = 14) -> Font {
+            Font.custom("Pretendard-Medium", size: size)
         }
         
-        static func captionBold() -> Font {
-            Font.custom("Pretendard-Bold", size: 12)
+        static func captionBold(size: CGFloat = 12) -> Font {
+            Font.custom("Pretendard-Bold", size: size)
         }
         
-        static func captionMedium() -> Font {
-            Font.custom("Pretendard-Medium", size: 12)
+        static func captionMedium(size: CGFloat = 12) -> Font {
+            Font.custom("Pretendard-Medium", size: size)
         }
     }
 }
