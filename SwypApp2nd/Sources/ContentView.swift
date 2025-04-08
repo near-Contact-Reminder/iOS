b50ef31af67c714b8466d260d196af6624855f06
jjwon2149 <jjwon2149@gmail.com>
2025-04-08 17:16:57 +0900

    feat: 앱 화면 Flow Enum으로 관리

--- SwypApp2nd/Sources/ContentView.swift
+++ SwypApp2nd/Sources/ContentView.swift
@@ -2,10 +2,22 @@ import SwiftUI
 import KakaoSDKCommon
 import KakaoSDKAuth
 
+// TODO: - 연결후 파일로 분할
+enum AppStep {
+    case login
+    case terms
+    case registerFriends
+    case setFrequency
+    case home
+}
+
 public struct ContentView: View {
     @StateObject private var userSession = UserSession.shared
     @StateObject private var loginViewModel = LoginViewModel()
     @StateObject private var homeViewModel = HomeViewModel()
+    @StateObject private var termsViewModel = TermsViewModel()
+    @StateObject private var registerFriendsViewModel = RegisterFriendsViewModel()
+    @StateObject private var contactFrequencyViewModel = ContactFrequencySettingsViewModel()
     
     public init() {
         // Kakao SDK 초기화
@@ -14,19 +26,46 @@ public struct ContentView: View {
 
     public var body: some View {
         Group {
-            if userSession.isLoggedIn {
-                HomeView(homeViewModel: homeViewModel)
-                    .transition(.move(edge: .leading))
-                    
-            } else {
+            switch userSession.appStep {
+            case .login, .terms:
                 LoginView(loginViewModel: loginViewModel)
-                    .transition(.move(edge: .leading))
+
+            case .registerFriends:
+                RegisterFriendView(viewModel: registerFriendsViewModel, proceed: {
+                    contactFrequencyViewModel.setPeople(from: registerFriendsViewModel.selectedContacts) // 선택된 연락처 전달
+                    userSession.appStep = .setFrequency
+                }, skip: {
+                    userSession.appStep = .home
+                })
+
+            case .setFrequency:
+                ContactFrequencySettingsView(viewModel: contactFrequencyViewModel, back: {
+                    userSession.appStep = .registerFriends
+                }, complete: {
+                    // TODO: - BackEnd 서버에 친구 목록 전달해주는 API 호출 필요
+                    userSession.appStep = .home
+                })
+
+            case .home:
+                HomeView(homeViewModel: homeViewModel)
             }
+        }
+        .sheet(isPresented: Binding<Bool>(
+            get: { userSession.appStep == .terms },
+            set: { if !$0 { userSession.appStep = .registerFriends } }
+        )) {
+            TermsView(viewModel: termsViewModel) {
+                DispatchQueue.main.async {
+                    userSession.appStep = .registerFriends
+                }
+            }
+            .presentationDetents([.medium])
+            .presentationDragIndicator(.visible)
         }
         .onAppear {
             userSession.tryAutoLogin()
         }
-        .animation(.easeInOut(duration: 0.4), value: userSession.isLoggedIn)
+        .animation(.easeInOut(duration: 0.4), value: userSession.appStep)
         .environmentObject(userSession)
     }
 }
