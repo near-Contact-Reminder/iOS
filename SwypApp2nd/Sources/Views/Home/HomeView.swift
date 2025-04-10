f6f914b8146777293d7b58e9da1a5aabec35d341
jjwon2149 <jjwon2149@gmail.com>
2025-04-10 18:03:04 +0900

    refactor: PreView 추가, indent 정리

--- SwypApp2nd/Sources/Views/Home/HomeView.swift
+++ SwypApp2nd/Sources/Views/Home/HomeView.swift
@@ -16,66 +16,73 @@ public struct HomeView: View {
     
     
     public var body: some View {
-            VStack {
-                // MARK: - Test
-                ForEach(notificationViewModel.reminders, id: \.self) { reminder in
-                }
-                
-                Button(action: {
-                    notificationViewModel.deleteAllReminders()
-                }) {
-                    Label("전체 삭제하기", systemImage: "trash")
-                }
-                    
-                if userSession.user?.loginType == .kakao {
-                    // 카카오 로그아웃 버튼
-                    Button {
-                        UserApi.shared.logout {(error) in
-                            if let error = error {
-                                print(error)
-                            }
-                            else {
-                                print("kakao logout success.")
-                                userSession.kakaoLogout()
-                            }
-                        }
-                    } label: {
-                        Text("카카오 로그아웃")
-                    }
-                } else {
-                    // apple 로그아웃 버튼
-                    Button {
-                        userSession.appleLogout()
-                    } label: {
-                        Text("애플 로그아웃")
-                    }
-                }
-                Spacer()
+        VStack {
+            // MARK: - Test
+            ForEach(notificationViewModel.reminders, id: \.self) { reminder in
             }
-            .navigationTitle("홈")
-            .toolbar {
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    ZStack(alignment: .topTrailing) {
-                        Button(action: {
-                            path.append(.inbox)
-                        }) {
-                            Image(systemName: "bell")
-                                .font(.title2)
+                
+            Button(action: {
+                notificationViewModel.deleteAllReminders()
+            }) {
+                Label("전체 삭제하기", systemImage: "trash")
+            }
+                    
+            if userSession.user?.loginType == .kakao {
+                // 카카오 로그아웃 버튼
+                Button {
+                    UserApi.shared.logout {(error) in
+                        if let error = error {
+                            print(error)
                         }
+                        else {
+                            print("kakao logout success.")
+                            userSession.kakaoLogout()
+                        }
+                    }
+                } label: {
+                    Text("카카오 로그아웃")
+                }
+            } else {
+                // apple 로그아웃 버튼
+                Button {
+                    userSession.appleLogout()
+                } label: {
+                    Text("애플 로그아웃")
+                }
+            }
+            Spacer()
+        }
+        .navigationTitle("홈")
+        .toolbar {
+            ToolbarItem(placement: .navigationBarTrailing) {
+                ZStack(alignment: .topTrailing) {
+                    Button(action: {
+                        path.append(.inbox)
+                    }) {
+                        Image(systemName: "bell")
+                            .font(.title2)
+                    }
                         
-                        if notificationViewModel.badgeCount > 0 {
-                            Text("\(notificationViewModel.badgeCount)")
-                                .font(.caption2)
-                                .foregroundColor(.white)
-                                .padding(5)
-                                .background(Color.red)
-                                .clipShape(Circle())
-                                .offset(x: 10, y: -10)
-                        }
+                    if notificationViewModel.badgeCount > 0 {
+                        Text("\(notificationViewModel.badgeCount)")
+                            .font(.caption2)
+                            .foregroundColor(.white)
+                            .padding(5)
+                            .background(Color.red)
+                            .clipShape(Circle())
+                            .offset(x: 10, y: -10)
                     }
                 }
             }
         }
     }
+}
 
-
+#Preview {
+    HomeView(
+        homeViewModel: HomeViewModel(),
+        notificationViewModel: NotificationViewModel(),
+        path: .constant([])
+    )
+    .environmentObject(UserSession.shared)
+}
