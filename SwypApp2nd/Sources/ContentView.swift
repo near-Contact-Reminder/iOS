dc7514acfa8e4813edc71101e5917892c1c26c25
JongWon Jeong <jjwon214@gmail.com>
2025-04-18 16:13:20 +0900

    refactor: ContentView 디버깅 추가, 주기 설정뷰의 people 홈뷰 people로 넘겨주는 코드 삭제

--- SwypApp2nd/Sources/ContentView.swift
+++ SwypApp2nd/Sources/ContentView.swift
@@ -43,6 +43,7 @@ public struct ContentView: View {
             case .registerFriends:
                 RegisterFriendView(viewModel: registerFriendsViewModel, proceed: {
                     contactFrequencyViewModel.setPeople(from: registerFriendsViewModel.selectedContacts) // 선택된 연락처 전달
+                    print("🟢 [RegisterFriendsViewModel] \(registerFriendsViewModel.selectedContacts) 전달됨")
                     userSession.appStep = .setFrequency
                 }, skip: {
                     userSession.appStep = .home
@@ -54,7 +55,7 @@ public struct ContentView: View {
                 }, complete: { updatedPeoples in
                     DispatchQueue.main.async {
                         print("🟢 [ContactFrequencySettingsView] 전달받은 people: \(updatedPeoples.map { $0.name })")
-                        homeViewModel.peoples = updatedPeoples
+//                        homeViewModel.peoples = updatedPeoples
                         UserSession.shared.user?.friends = updatedPeoples
                         userSession.appStep = .home
                     }
