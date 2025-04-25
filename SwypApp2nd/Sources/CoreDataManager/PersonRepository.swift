05383d25fa151ff1272bddd1bc2e44099d1b03ab
Sharon Kang <kang26d@mtholyoke.edu>
2025-04-25 00:07:16 -0700

    chore: 필요없는 personEntity 관련 함수 삭제

--- SwypApp2nd/Sources/CoreDataManager/PersonRepository.swift
+++ SwypApp2nd/Sources/CoreDataManager/PersonRepository.swift
@@ -1,24 +1,7 @@
 import CoreData
 
 class PersonRepository {
-//    private let context = CoreDataStack.shared.context
-//    
-//    func fetchPeople() -> [PersonEntity] {
-//        let request: NSFetchRequest<PersonEntity> = PersonEntity.fetchRequest()
-//        do {
-//            return try context.fetch(request)
-//        } catch {
-//            print("사람 목록 불러오기 실패: \(error.localizedDescription)")
-//            return []
-//        }
-//    }
-//    
-//        func deletePerson(_ person: PersonEntity) {
-//            context.delete(person)
-//            saveContext()
-//        }
-//
-//    private func saveContext() {
-//        CoreDataStack.shared.saveContext()
-//    }
+    
+  
+
 }
