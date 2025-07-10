2de27d3a4818bf35eb86da80864e4accb0292906
JongWon Jeong <jjwon2149@gmail.com>
2025-07-11 00:24:59 +0900

    fix: HomeView GreetingSection 텍스트 some View issue 해결

--- SwypApp2nd/Sources/Views/Home/HomeView.swift
+++ SwypApp2nd/Sources/Views/Home/HomeView.swift
@@ -146,12 +146,11 @@ struct GreetingSection: View {
                 .modifier(Font.Pretendard.h1MediumStyle())
                 .foregroundColor(.white)
             HStack(spacing: 0) {
-                Text(message1)
-                    .modifier(Font.Pretendard.h1BoldStyle())
-                    .foregroundColor(.white)
-                Text(message2)
-                    .modifier(Font.Pretendard.h1MediumStyle())
-                    .foregroundColor(.white)
+                (Text(message1)
+                    .font(Font.Pretendard.h1Bold())
+                + Text(message2)
+                    .font(Font.Pretendard.h1Medium()))
+                .foregroundColor(.white)
             }
         }
         .padding(.horizontal, 24)
